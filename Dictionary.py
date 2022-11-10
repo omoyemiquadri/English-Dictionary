efb62c39584dde6de9b14889783fbb88d65932aa
Omoyemi Quadri <56293002+omoyemiquadri@users.noreply.github.com>
2022-11-10 21:36:54 +0100

    Add files via upload
    Dictionary is the world's leading online source for definitions, word origins, and a whole lot more.

---
+++ Dictionary.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+def dictionary(word):
+    page = requests.get( 'https://www.dictionary.com/browse/'+word)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    site = soup.find_all('div', {'value': '1'})
+    print(site[0].get_text())
+    
+word = input('Enter your search term: ')
+dictionary(word)
+print('Do you want to search again?')
+ 
+opt = input('yes or no: ')
+if opt == 'yes':
+     word= input('Enter your search term: ')
+     dictionary(word)
+     while opt == 'yes':
+         opt = input('yes or no: ')
+         word= input('Enter your search term: ')
+         dictionary(word)
+         if opt == 'no':
+             print('Thanks and have a nice day')
+quit()
