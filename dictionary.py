5c8ce55e36e0f70084af1ad091faa6390f279416
Omoyemi Quadri <56293002+omoyemiquadri@users.noreply.github.com>
2022-11-11 05:52:22 -0600

    Added an app banner

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,8 @@
 from lib.dictionary_class import Dict
+import pyfiglet as fg
+
+label = fg.figlet_format("DICTIONARY")
+print(f"{label} \n\t\t ONLINE ENGLISH DICTIONARY.")
 
 obj = Dict()
 obj.run()
-
-# pip install pyinstaller
-# pyinstaller --onefile file.py
